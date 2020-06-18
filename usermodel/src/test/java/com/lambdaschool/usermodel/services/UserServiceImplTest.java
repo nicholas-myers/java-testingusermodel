8c762ae44731849f447a6db85ad82b9ef461eed6
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-06-18 15:07:13 -0700

    set up the find user id, find name, find all user tests

--- usermodel/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
+++ usermodel/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
@@ -1,17 +1,34 @@
 package com.lambdaschool.usermodel.services;
 
+import com.lambdaschool.usermodel.UserModelApplication;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityNotFoundException;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserModelApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserServiceImplTest
 {
+
+   @Autowired
+   private UserService userService;
 
    @Before
    public void setUp() throws Exception
    {
+      MockitoAnnotations.initMocks(this);
    }
 
    @After
@@ -19,53 +36,78 @@ public class UserServiceImplTest
    {
    }
 
+   @Test()
+   public void afindUserById()
+   {
+      assertEquals("test admin", userService.findUserById(4).getUsername());
+   }
+
+   @Test(expected = EntityNotFoundException.class)
+   public void aafindUserByIdFail()
+   {
+      assertEquals("test admin", userService.findUserById(1).getUsername());
+   }
+
    @Test
-   public void findUserById()
+   public void bfindByNameContaining()
    {
    }
 
    @Test
-   public void findByNameContaining()
+   public void cfindAll()
+   {
+      assertEquals(30, userService.findAll().size());
+   }
+
+   @Test
+   public void ddelete()
+   {
+      userService.delete(4);
+      assertEquals(29, userService.findAll().size());
+   }
+
+   @Test (expected = EntityNotFoundException.class)
+   public void dadeletenotfound()
+   {
+      userService.delete(1);
+      assertEquals(29, userService.findAll().size());
+   }
+
+   @Test
+   public void efindByName()
+   {
+      assertEquals("test admin", userService.findByName("test admin").getUsername());
+   }
+
+   @Test(expected = EntityNotFoundException.class)
+   public void eafindByNameFail()
+   {
+      assertEquals("the boss", userService.findByName("the boss").getUsername());
+   }
+
+   @Test
+   public void fsave()
    {
    }
 
    @Test
-   public void findAll()
+   public void gupdate()
    {
    }
 
    @Test
-   public void delete()
+   public void hgetCountUserEmails()
+   {
+      assertEquals(2, userService.findUserById(4).getUseremails().size());
+   }
+
+   @Test
+   public void ideleteUserRole()
    {
    }
 
    @Test
-   public void findByName()
-   {
-   }
-
-   @Test
-   public void save()
-   {
-   }
-
-   @Test
-   public void update()
-   {
-   }
-
-   @Test
-   public void getCountUserEmails()
-   {
-   }
-
-   @Test
-   public void deleteUserRole()
-   {
-   }
-
-   @Test
-   public void addUserRole()
+   public void jaddUserRole()
    {
    }
 }
